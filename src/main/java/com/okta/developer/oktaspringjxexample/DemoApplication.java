d2d2b43611f22988ad181eca05e0ab687055178c
Efren Garcia Home <efren.gl@gmail.com>
2019-01-29 22:21:40 -0500

    logger PR-test

--- src/main/java/com/okta/developer/oktaspringjxexample/DemoApplication.java
+++ src/main/java/com/okta/developer/oktaspringjxexample/DemoApplication.java
@@ -2,11 +2,16 @@ package com.okta.developer.oktaspringjxexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("An INFO Message XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
